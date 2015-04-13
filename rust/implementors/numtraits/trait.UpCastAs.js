75fed340db3cd9c145b2cd1cacc8a0f917cb1b0d
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2015-04-13 03:59:29 -0700

    Initial commit. Rustdocs.

---
+++ rust/implementors/numtraits/trait.UpCastAs.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['numtraits'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rust/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['numtraits'] = {"items":[[0,"","numtraits","This module defines a trait `UpCastAs<T>` which allows one to upcast (as in only types which make sense\nand can fit it another are allowed) between primitive types. These follow a simple hierarchy:",null,null],[5,"cast","","",null,{"inputs":[{"name":"v"}],"output":{"name":"t"}}],[8,"UpCastAs","","",null,null],[10,"from","","",0,{"inputs":[{"name":"upcastas"},{"name":"t"}],"output":{"name":"self"}}]],"paths":[[8,"UpCastAs"]]};
+initSearch(searchIndex);
